dc02d272c4a1a7fbd171c5d133eb5da06bdfeac2
Victor Igor <victor.0w3@hotmail.com>
2016-07-26 23:17:39 -0400

    intro user

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160727015459) do
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name",       limit: 255, null: false
+    t.string   "email",      limit: 255, null: false
+    t.string   "login",      limit: 255, null: false
+    t.string   "password",   limit: 255, null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["login"], name: "index_users_on_login", unique: true, using: :btree
 
 end

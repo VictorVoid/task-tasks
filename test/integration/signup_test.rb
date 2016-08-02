9b3e398cd20287d5c1bf2b57e7e04e3391fea0bd
Greg Hurrell <victor.0w3@hotmail.com>
2016-08-01 23:15:35 -0400

    test for empty fields

--- test/integration/signup_test.rb
+++ test/integration/signup_test.rb
@@ -1,17 +1,25 @@
 require 'test_helper'
 class SignupTest < ActionDispatch::IntegrationTest
-  test "sign up with valid data" do
-    visit "/"
-    click_link "Quero me cadastrar"
+    test "sign up with valid data" do
+        visit "/"
+        click_link "Quero me cadastrar"
 
-    fill_in "Seu nome", with: "John Doe"
-    fill_in "Seu e-mail", with: "John Doe"
-    fill_in "Seu login", with: "johndoe"
-    fill_in "Sua senha", with: "test"
-    fill_in "Confirme sua senha", with: "test"
-    click_button "Quero me cadastrar"
-  ## espero que aconteca
-    assert_equal "/login", current_path #espera que eles sao iguais
-    assert page.has_content?("Cadastrado realizado com sucesso!") #espera que o valor seja verdadeiro
+        fill_in "Seu nome", with: "John Doe"
+        fill_in "Seu e-mail", with: "John Doe"
+        fill_in "Seu login", with: "johndoe"
+        fill_in "Sua senha", with: "test"
+        fill_in "Confirme sua senha", with: "test"
+        click_button "Quero me cadastrar"
+      ## espero que aconteca
+        assert_equal "/login", current_path #espera que eles sao iguais
+        assert page.has_content?("Cadastrado realizado com sucesso!") #espera que o valor seja verdadeiro
+    end
+    test "sign up with invalid data" do
+        visit "/"
+        click_link "Quero me cadastrar"
+        click_button "Quero me cadastrar"
+
+        assert_equal "/signup", current_path # ou seja, espera que ele continue na mesma página, avisando o erro 
+        assert page.has_content?("Verifique o formulário antes de continuar") #espera que o valor seja verdadeiro
     end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
+# No rails eu posso simplesmente fazer testes só no models quando quero.
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+#teste para validar
+	test "require name" do 
+		#se instanciar um objeto como o nome nulo, e salvar...
+		user = User.new(name: nil)
+		user.save
+
+		assert user.errors[:name] #eu espero que o usuario tenha erros no attr name
+	end
+
 end

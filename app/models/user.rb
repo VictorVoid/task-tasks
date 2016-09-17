2e97c0642309a87d9d0f3295446f7db60122be82
Victor Igor <victor.0w3@hotmail.com>
2016-09-16 23:58:28 -0400

    validate user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
 	attr_accessor :password_confirmation
+
+  validates_presence_of :name, :email
+  validates_format_of :email, with: /\A[a-z0-9.]+@[a-z0-9.-]+\.[a-z]{2,4}\z/
+  # validates :name, presence: true
+
 end
+ 
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 require 'test_helper'
 
 # No rails eu posso simplesmente fazer testes só no models quando quero.
@@ -5,10 +6,29 @@ class UserTest < ActiveSupport::TestCase
 #teste para validar
 	test "require name" do 
 		#se instanciar um objeto como o nome nulo, e salvar...
-		user = User.new(name: nil)
-		user.save
-
+		user = User.create(name: nil)
 		assert user.errors[:name] #eu espero que o usuario tenha erros no attr name
 	end
 
+  test "requires email" do
+    user = User.create(email: nil)
+    assert user.errors[:email].any?
+  end
+
+
+  test "requires valid email" do
+    user = User.create(email: "invalid")
+    assert user.errors[:email].any?
+  end
+
+  %w[
+"john@example.org"
+john@example.me
+jogn@example-domain.com
+  ].each do |email|
+    test "accepts #{email}" do
+      user = User.create(email: email)
+      assert user.errors[:email].empty?
+    end
+  end
 end
